5d5672bfd31836b9cfdd382b5004233fa4edd964
Anna <equestrian.sport.lovely@gmail.com>
2025-03-04 00:41:33 +0300

    commit

---
+++ Models/Data.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangletion.Models
+{
+    internal class Data
+    {
+        public static List<Router> Routers = new List<Router>();
+
+        public static Priemnik Priemnik = new Priemnik();
+
+        //корень((x2 - x1)² + (y2 - y1)²)
+        public static void CalculateDistance() //расстояние от роутера до приемника
+        {
+            foreach (var router in Routers)
+            {
+                router.Distance = Math.Sqrt(
+                    Math.Pow((router.Coordinates.X - Priemnik.Coordinates.X), 2) + 
+                    Math.Pow((router.Coordinates.Y - Priemnik.Coordinates.Y),2)
+                    );
+            }
+        }
+    }
+}
---
+++ Models/Priemnik.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Triangletion.Models
+{
+    internal class Priemnik
+    {
+        public Point Coordinates { get; set; }
+    }
+}
